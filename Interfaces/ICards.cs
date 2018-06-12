9b665a2d084955923784628be866c7037e096bfb
FernanPro18 <fernandoaraujo081@gmail.com>
2018-06-11 20:23:54 -0500

    Arreglo interface

--- Interfaces/ICards.cs
+++ Interfaces/ICards.cs
@@ -7,10 +7,40 @@ namespace eCommerce_Csharp_Cards.Interfaces
 {
     public interface ICards
     {
-    Task<IEnumerable<Cards>> Get();
-    Task<Cards> Get(string _id);
-    Task Add(Cards user);
-    Task<ReplaceOneResult> Update(string _id, Cards Card);
-    Task<DeleteResult> Remove(string _id);
+    Task<IEnumerable<Cards>> GetAmazon();
+    Task<IEnumerable<Cards>> GetGooglePlay();
+    Task<IEnumerable<Cards>> GetiTunes();
+    Task<IEnumerable<Cards>> GetSteam();
+    Task<IEnumerable<Cards>> GetPlayStation();
+    Task<IEnumerable<Cards>> GetXbox();
+    Task<IEnumerable<Cards>> GetPaypal();
+    Task<Cards> GetAmazon(string _id);
+    Task<Cards> GetGooglePlay(string _id);
+    Task<Cards> GetiTunes(string _id);
+    Task<Cards> GetSteam(string _id);
+    Task<Cards> GetPlayStation(string _id);
+    Task<Cards> GetXbox(string _id);
+    Task<Cards> GetPaypal(string _id);
+    Task PostAmazon(Cards Card);
+    Task PostGooglePlay(Cards Card);
+    Task PostiTunes(Cards Card);
+    Task PostSteam(Cards Card);
+    Task PostPlayStation(Cards Card);
+    Task PostXbox(Cards Card);
+    Task PostPaypal(Cards Card);
+    Task<ReplaceOneResult> PutAmazon(string _id, Cards Card);
+    Task<ReplaceOneResult> PutGooglePlay(string _id, Cards Card);
+    Task<ReplaceOneResult> PutiTunes(string _id, Cards Card);
+    Task<ReplaceOneResult> PutSteam(string _id, Cards Card);
+    Task<ReplaceOneResult> PutPlayStation(string _id, Cards Card);
+    Task<ReplaceOneResult> PutXbox(string _id, Cards Card);
+    Task<ReplaceOneResult> PutPaypal(string _id, Cards Card);
+    Task<DeleteResult> DeleteAmazon(string _id);
+    Task<DeleteResult> DeleteGooglePlay(string _id);
+    Task<DeleteResult> DeleteiTunes(string _id);
+    Task<DeleteResult> DeleteSteam(string _id);
+    Task<DeleteResult> DeletePlayStation(string _id);
+    Task<DeleteResult> DeleteXbox(string _id);
+    Task<DeleteResult> DeletePaypal(string _id);
     }
 }
---
+++ Repositorio/Card_Repositorio.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using eCommerce_Csharp_Cards.Interfaces;
+using eCommerce_Csharp_Cards.Models;
+using MongoDB.Driver;
+
+namespace eCommerce_Csharp_Cards
+{
+  public class Card_Repositorio : ICards
+  {
+  }
+}
