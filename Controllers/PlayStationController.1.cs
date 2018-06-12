807b8d8c3c9a340b8938fe3e1898a9c1b2802b53
FernanPro18 <fernandoaraujo081@gmail.com>
2018-06-11 21:30:59 -0500

    Controllers Archivos

--- Controllers/PlayStationController.1.cs
+++ Controllers/PlayStationController.1.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce_Csharp_Cards.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [Authorize]
+    [Route("Cards/[controller]")]
+    public class PlayStationController : Controller
     {
-        // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
-
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
