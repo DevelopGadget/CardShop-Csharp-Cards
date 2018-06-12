9eeb6fd6705c1891d258585c9aef7ac6769bd43f
FernanPro18 <fernandoaraujo081@gmail.com>
2018-06-12 11:23:43 -0500

    Startup

--- Startup.cs
+++ Startup.cs
@@ -1,40 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using eCommerce_Csharp_Cards.Interfaces;
+using eCommerce_Csharp_Cards.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
-namespace eCommerce_Csharp_Cards
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace eCommerce_Csharp_Cards {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer (options => {
+                    options.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Issuer"].ToString (),
+                    ValidAudience = Configuration["Issuer"].ToString (),
+                    IssuerSigningKey = new SymmetricSecurityKey (Encoding.UTF8.GetBytes (Configuration["Key"]))
+                    };
+                });
+            services.Configure<Settings> (o => { o.configuration = Configuration; });
+            services.AddTransient<ICards, Card_Repositorio> ();
+            services.AddMvc ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
-
-            app.UseMvc();
+            app.UseAuthentication ();
+            app.UseCors (data =>
+                data.AllowAnyHeader ().AllowAnyMethod ().AllowAnyOrigin ());
+            app.UseMvc (Routes => {
+                Routes.MapRoute (
+                    name: "Default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
