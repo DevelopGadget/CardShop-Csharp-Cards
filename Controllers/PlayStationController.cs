cf6670d7ab09d5b01f1eafa3b6c471e9d9256323
FernanPro18 <fernandoaraujo081@gmail.com>
2018-06-12 10:26:10 -0500

    Play Station

--- Controllers/PlayStationController.cs
+++ Controllers/PlayStationController.cs
@@ -2,43 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eCommerce_Csharp_Cards.Interfaces;
+using eCommerce_Csharp_Cards.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
-namespace eCommerce_Csharp_Cards.Controllers
-{
+namespace eCommerce_Csharp_Cards.Controllers {
     [Authorize]
-    [Route("Cards/[controller]")]
-    public class PlayStationController : Controller
-    {
+    [Route ("Cards/[controller]")]
+    public class PlayStationController : Controller {
+        private readonly ICards _Cards;
+        public PlayStationController (ICards Cards) => this._Cards = Cards;
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
+        public async Task<IActionResult> Get () {
+            try {
+                IEnumerable<Cards> Cards = await _Cards.GetiTunes ();
+                if (Cards == null) return StatusCode (StatusCodes.Status406NotAcceptable, "No Hay Documentos");
+                return Ok (JsonConvert.SerializeObject (Cards));
+            } catch (Exception) {
+                return BadRequest ("Error vuelva a intentar");
+            }
         }
 
-        // POST api/values
+        [HttpGet ("{id}")]
+        public async Task<IActionResult> Get (string Id) {
+            try {
+                if (string.IsNullOrEmpty (Id) || Id.Length < 24) return StatusCode (StatusCodes.Status406NotAcceptable, "Id Invalid");
+                Cards Card = await _Cards.GetiTunes (Id);
+                if (Card == null) return StatusCode (StatusCodes.Status406NotAcceptable, "No Hay Documentos");
+                return Ok (JsonConvert.SerializeObject (Card));
+            } catch (Exception) {
+                return BadRequest ("Error vuelva a intentar");
+            }
+        }
+
         [HttpPost]
-        public void Post([FromBody]string value)
-        {
+        public async Task<IActionResult> Post ([FromBody] Cards Card) {
+            try {
+                if (!ModelState.IsValid) return StatusCode (StatusCodes.Status406NotAcceptable, ModelState);
+                await _Cards.PostiTunes (Card);
+                return Ok ("Creado");
+            } catch (Exception) {
+                return BadRequest ("Ha Ocurrido Un Error Vuelva A Intentar");
+            }
         }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
+        [HttpPut ("{id}")]
+        public async Task<IActionResult> Put (string Id, [FromBody] Cards Card) {
+            try {
+                if (string.IsNullOrEmpty (Id) || Id.Length < 24) return StatusCode (StatusCodes.Status406NotAcceptable, "Id Invalid");
+                Cards CardId = await _Cards.GetiTunes (Id);
+                if (CardId == null) return StatusCode (StatusCodes.Status406NotAcceptable, "No Hay Documentos");
+                if (!ModelState.IsValid) return StatusCode (StatusCodes.Status406NotAcceptable, ModelState);
+                Card.Id = Id;
+                var h = await _Cards.PutiTunes (Id, Card);
+                if (h.MatchedCount > 0) return Ok ("Editado");
+                else return StatusCode (StatusCodes.Status406NotAcceptable, "No Editado");
+            } catch (Exception) {
+                return BadRequest ("Ha Ocurrido Un Error Vuelva A Intentar");
+            }
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+        [HttpDelete ("{id}")]
+        public async Task<IActionResult> Delete (string Id) {
+            try {
+                if (string.IsNullOrEmpty (Id) || Id.Length < 24) return StatusCode (StatusCodes.Status406NotAcceptable, "Id Invalid");
+                var h = await _Cards.DeleteiTunes (Id);
+                if (h.DeletedCount > 0) return Ok ("Eliminado");
+                else return StatusCode (StatusCodes.Status406NotAcceptable, "No Eliminado");
+            } catch (Exception) {
+                return BadRequest ("Ha Ocurrido Un Error Vuelva A Intentar");
+            }
         }
     }
 }
